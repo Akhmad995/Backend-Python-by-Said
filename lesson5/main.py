88dc5707278a2cf49d7c0cd559577123b65b9a2d
Akhmad995 <axmed-005@mail.ru>
2023-11-21 21:06:59 +0300

    update lesson 5

--- lesson5/main.py
+++ lesson5/main.py
@@ -4,8 +4,6 @@ from flask import render_template, request, redirect, url_for
 
 
 app = Flask(__name__)
-
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my_db.db'
 db = SQLAlchemy(app)
 
@@ -16,11 +14,15 @@ class User(db.Model):
     username = db.Column(db.String(88), unique=True, nullable=False)
     # Распространные типы - Integer, Text, Boolean, Datetime, Float, LargeBinary
 
+    def __repr__(self):
+        return f"<User id={self.id}>, username={self.username}"
+
 
 @app.route('/')
 def index():
     users = User.query.all()
-    return render_template('index.html')
+    print(users)
+    return render_template('index.html', users=users)
 
 
 @app.route('/add_user', methods=['POST'])
@@ -29,9 +31,33 @@ def add_user():
     user = User(username=username)
     db.session.add(user)
     db.session.commit()
+    print(user)
 
     return redirect( url_for('index') )
 
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), unique=True, nullable=False)
+    text = db.Column(db.String, nullable=False)
+
+
+@app.route('/post', methods=['POST', 'GET'])
+def add_post():
+    if request.method == "POST":
+        title = request.form.get("title")
+        text = request.form.get("text")
+        
+        post = Post(title=title, text=text)
+        
+        db.session.add(post)
+        db.session.commit()
+        print(post)
+
+        return redirect( url_for('add_post') )
+    posts = Post.query.all()
+    return render_template('post.html', posts=posts)
 
 
 if __name__ == '__main__':
